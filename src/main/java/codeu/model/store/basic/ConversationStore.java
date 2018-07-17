18f4b0614f1a3f0dc4a9b5475b3240ebc6559b30
Christopher Avalos <cavalos99@Christophers-MacBook-Air.local>
2018-07-16 18:51:18 -0700

    Merging

--- src/main/java/codeu/model/store/basic/ConversationStore.java
+++ src/main/java/codeu/model/store/basic/ConversationStore.java
@@ -15,7 +15,6 @@
 package codeu.model.store.basic;
 
 import codeu.model.data.Conversation;
-import codeu.model.data.Message;
 import codeu.model.store.persistence.PersistentStorageAgent;
 import java.util.ArrayList;
 import java.util.List;
